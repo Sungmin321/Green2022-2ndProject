b2d258c87fe86c60afe9572667104d3bf3235735
Administrator <Administrator@User -2022TSFVF>
2022-07-28 09:29:10 +0900

    7/28, 09:28 확인

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("°­½ÂÁø");
 		System.out.println("µµÀº¼³");
 		System.out.println("ÇÔ¼º¹Î");
 		System.out.println("±èÅÂ±Ô");
+		
+		System.out.println("seungjin ---- 2022-07-28, 09:28");
 	}
 }
